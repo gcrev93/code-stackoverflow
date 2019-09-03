936934fa4b73944b9a8b9078d48927a4bb9e6d72
Gabrielle Crevecoeur <gcrev93@gmail.com>
2019-09-03 12:45:04 -0500

    small typescript updates

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as selection from './selection';
-
-var open  = require('open');
+import open  = require('open');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,7 +12,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	var search = vscode.commands.registerCommand('extension.searchStack', () => {
+	let search = vscode.commands.registerCommand('extension.searchStack', () => {
 		// The code you place here will be executed every time your command is executed
 
 		vscode.window.showInputBox({
@@ -26,14 +25,14 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 	
 	//use Stack Overflow to search selected text
-	var s_search = vscode.commands.registerTextEditorCommand('extension.searchSelection', (textEdtior: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
+	let s_search = vscode.commands.registerTextEditorCommand('extension.searchSelection', (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
 		let thisSelection = {
-			start: textEdtior.selections[0].start, 
-			end: textEdtior.selections[0].end
+			start: textEditor.selections[0].start, 
+			end: textEditor.selections[0].end
 		};
 		
 		//Use the node module "open" to open a web browser
-		let output = selection.getStringFromSelection(thisSelection, textEdtior.document);
+		let output = selection.getStringFromSelection(thisSelection, textEditor.document);
 		if(output && output.trim.length > 0){
 			open('http://stackoverflow.com/search?q='+output);
 		}
